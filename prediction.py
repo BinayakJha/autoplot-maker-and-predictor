f77d666481135e766625bdc44617d0406bf70e02
Binayak Jha <binayakjha@Binayak-MacBook-Air.local>
2021-08-09 20:05:09 +0545

    Made whole program Terminal based and more easy to use .

--- prediction.py
+++ prediction.py
@@ -7,6 +7,6 @@ def predicts(df,X,y,x_label,y_label):
     model = LinearRegression()
     model.fit(df[[X]], df[[y]])
     # ask a value to predict
-    predict_value = input(text = 'Please enter a value  of '+str(x_label)+' to predict '+str(y_label))
+    predict_value = input('Please enter a value  of '+str(x_label)+' to predict '+str(y_label))
     predict = model.predict([[predict_value]])
     print('Predicted value of '+ str(y_label)+ ' is ' + str(int(predict)))
